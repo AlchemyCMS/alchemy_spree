b15f1d0b9f739d6953da7fc11bc210f21ffce389
Thomas von Deyen <tvd@magiclabs.de>
2015-04-09 20:26:03 +0200

    Update esssences preview methods.

--- app/models/alchemy/essence_spree_product.rb
+++ app/models/alchemy/essence_spree_product.rb
@@ -1,10 +1,11 @@
 module Alchemy
   class EssenceSpreeProduct < ActiveRecord::Base
     belongs_to :product, class_name: "Spree::Product", foreign_key: 'spree_product_id'
+    delegate :name, to: :product
 
     acts_as_essence(
       ingredient_column: 'spree_product_id',
-      preview_text_method: 'name'
+      preview_text_column: 'name'
     )
 
     def ingredient
--- app/models/alchemy/essence_spree_taxon.rb
+++ app/models/alchemy/essence_spree_taxon.rb
@@ -1,10 +1,11 @@
 module Alchemy
   class EssenceSpreeTaxon < ActiveRecord::Base
     belongs_to :taxon, class_name: "Spree::Taxon", foreign_key: 'taxon_id'
+    delegate :name, to: :taxon
 
     acts_as_essence(
       ingredient_column: 'taxon_id',
-      preview_text_method: 'name'
+      preview_text_column: 'name'
     )
 
     def ingredient
