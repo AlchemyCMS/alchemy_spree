aef9aee421877743b44694d47254a51c49f86199
Patrice Lebel <plebel@o2web.ca>
2015-06-06 21:28:02 -0400

    allow guest user to view Alchemy content

--- lib/alchemy/spree/ability.rb
+++ lib/alchemy/spree/ability.rb
@@ -2,10 +2,13 @@ module Alchemy
   module Spree
     class Ability
       include CanCan::Ability
+      include Permissions::GuestUser
 
       def initialize(user)
         if user && user.alchemy_roles.include?('admin')
           can :index, :alchemy_admin_spree
+        else
+          alchemy_guest_user_rules
         end
       end
     end
